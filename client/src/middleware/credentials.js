0cb021d743ebc02ed4b1c3a693e5fa8d56a6352b
tsm98 <sairam.tm@gmail.com>
2021-03-21 20:30:23 +0530

    3 commit

---
+++ client/src/middleware/credentials.js
@@ -0,0 +1,8 @@
+//You need to update each credentials as displayed on your moibit api dashboard.
+
+export default {
+    "API_KEY": "12D3KooWCoifUL8s3gCFcefFQAj9zQJvVZsfWKDB3HiCEN3QjssW", //YOUR API KEY
+    "API_SECRET": "08011240dd6e9951c354442da9cdbc2d353e2f20df7c7b6213972aa8b13c079a451926c42c68a771fabb4c99959e02641788fc4d4cc263a1b508b4bef6f0d2319a2d8f79", //YOUR API SECRET
+    "ADMIN": "0x246A75bDBc1CdE906361f6dDF6f2179aBC5279Ff", // ANY ACCOUNT OF YOUR WISH
+    "CUSTOM_URL": "https://kfs4.moibit.io" //URL YOU HAVE BEEN ASSIGNED TO
+}
